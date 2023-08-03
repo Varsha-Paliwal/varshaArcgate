a0dc9aaf9924642d4668679e795ef353eaff988d
varsha911 <varshapaliwal911@gmail.com>
2023-08-03 18:17:39 +0530

    check in

--- arcCRM.js
+++ arcCRM.js
@@ -10,12 +10,12 @@
            optionsContainer.style.display = optionsContainer.style.display === 'block' ? 'none' : 'block';
        });
 
-       // Handle option selection and close the dropdown
+       //  option selection and close the dropdown
        optionBackgrounds.forEach(option => {
            option.addEventListener('click', () => {
                selectedOption.textContent = option.textContent;
                optionsContainer.style.display = 'none';
-               // Dispatch a click event on the custom dropdown to close it
+             
                customDropdown.dispatchEvent(new Event('click'));
            });
        });
@@ -27,3 +27,65 @@
            }
        });
    });
+
+   
+   
+   
+   function selectOption(option) {
+    const selectedOption = document.getElementById('selected-option');
+    selectedOption.textContent = option + '  \u2193';
+
+    const optionsContainer = document.getElementById('options-container');
+    optionsContainer.style.display = 'none';
+
+    if (option === 'Reject') {
+        afterAccpectopenModal("modalOpen");
+    }
+}
+
+
+function afterAccpectopenModal(modalName) {
+    // const inputReason = document.getElementById('inputReason').value;
+    // console.log('Rejected with reason:', inputReason);
+    const modal = document.getElementById(modalName);
+     modal.style.display = 'block';
+    modal.classList.add('show');
+   
+}
+
+function rejectAndCloseModal(modalName) {
+   
+    // const inputReason = document.getElementById('inputReason').value;
+    // console.log('Rejected with reason:', inputReason);
+    const modal = document.getElementById(modalName);
+     modal.style.display = 'none';
+    modal.classList.remove('show');
+    if(modalName !="actionModal"&& modalName !="rejectModal"){  
+        afterAccpectopenModal("rejectModal")
+}
+
+}
+
+
+function accpectAndCloseModal(modalName) {
+    console.log(modalName)
+    // const inputReason = document.getElementById('inputReason').value;
+    // console.log('Rejected with reason:', inputReason);
+    const modal = document.getElementById(modalName);
+     modal.style.display = 'none';
+    modal.classList.remove('show');
+    if(modalName !="modalforQuery"){
+        console.log(modalName)
+    afterAccpectopenModal("modalforQuery")}
+   
+    // Additional logic for handling the rejection
+}
+
+
+function selectModal (modalName){
+    const modal = document.getElementById(modalName);
+    modal.style.display = 'none';
+    modal.classList.remove('show');
+    afterAccpectopenModal("actionModal")
+}
+
